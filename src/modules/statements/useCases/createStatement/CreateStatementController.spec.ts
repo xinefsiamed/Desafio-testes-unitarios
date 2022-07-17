249c8be74d5e6a03269c9abad848458d0ef904cf
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-07-16 21:08:03 -0300

    adding withdraw tests for createStatementController

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -50,7 +50,7 @@ describe("Create Statement Controller", () => {
     expect(balanceResponse.body.balance).toBe(100)
   })
 
-  it("Should not be able to create a deposit without authorization", async () => {
+  it("Should not be able to create an deposit without authorization", async () => {
     const depositResponse = await request(app).post('/api/v1/statements/deposit').send({
       amount: 100,
       description: 'Test deposit'
@@ -58,5 +58,49 @@ describe("Create Statement Controller", () => {
 
     expect(depositResponse.status).toBe(401)
     expect(depositResponse.body.message).toBe('JWT token is missing!')
+  })
+
+  it("Should be able to create a withdraw", async () => {
+    await request(app).post('/api/v1/users').send({
+      name: 'test',
+      email: 'test@test.com',
+      password: 'password'
+    })
+
+    const tokenResponse = await request(app).post('/api/v1/sessions').send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+
+    const { token } = tokenResponse.body
+
+    await request(app).post('/api/v1/statements/deposit').send({
+      amount: 100,
+      description: 'Test deposit'
+    })
+
+    const withdrawResponse = await request(app).post('/api/v1/statements/withdraw').send({
+      amount: 50,
+      description: 'Test withdraw'
+    }).set({
+      Authorization: 'Bearer ' + token
+    })
+
+    const userBalance = await request(app).get('/api/v1/statements/balance').set({
+      Authorization: 'Bearer ' + token
+    })
+
+    expect(withdrawResponse.status).toBe(201)
+    expect(userBalance.body.balance).toBe(50)
+  })
+
+  it("Should not be able to create a withdraw without authorization", async () => {
+    const withdrawResponse = await request(app).post('/api/v1/statements/withdraw').send({
+      amount: 100,
+      description: 'Test withdraw'
+    })
+
+    expect(withdrawResponse.status).toBe(401)
+    expect(withdrawResponse.body.message).toBe('JWT token is missing!')
   })
 })
